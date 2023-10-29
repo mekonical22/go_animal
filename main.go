73bad16c8beb6ecc2662de581dfa33f5d501a99a
mekonical22 <mekonical222@gmail.com>
2023-10-29 11:51:01 +0800

    handle GET on "/"

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello, world!")
+	Index := func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			i := len(animalsData)-1
+			data := GetAnimalData(animalsData[i])
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(*data)
+			return
+		}
+
+		w.WriteHeader(http.StatusBadRequest) // reject all methods except GET
+		return
+	}
+	http.HandleFunc("/", Index)
+
+	http.ListenAndServe(":3000", nil)
 }
+
+var animalsData = [...]Animal{}
